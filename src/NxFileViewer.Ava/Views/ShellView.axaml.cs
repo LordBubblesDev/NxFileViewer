150b18934bde5a44d4c2ab246f56a013d999fdc6
ArchLeaders <archleadership28@gmail.com>
2025-09-09 12:14:26 -0700

    Add avalonia app

---
+++ src/NxFileViewer.Ava/Views/ShellView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NxFileViewer.Ava.Views;
+
+public partial class ShellView : Window
+{
+    public ShellView()
+    {
+        InitializeComponent();
+    }
+}
